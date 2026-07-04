e3dceb9a3a353008f64603c211754c7bf4eae439
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-07-04 11:02:05 -0500

    Add test splitting function

---
+++ testing.py
@@ -0,0 +1,9 @@
+def split(script: str) -> str:
+    "Break up the script into chunks"
+    return script.split(". ")
+
+#TODO: Add an option for "preserve period"
+
+if (__name__ == "__main__"):
+    r = split("The text: str syntax says that the text argument should be of type str. Similarly, the optional align argument should have type bool with the default value True. Finally, the -> str notation specifies that headline() will return a string.")
+    print(r)
