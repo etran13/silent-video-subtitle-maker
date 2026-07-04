2f25eb5290dd95cd5b9f511c2ce84250e2c7ba10
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-07-04 18:22:25 -0500

    Add text box and text-getting functionality to interface

--- interface.py
+++ interface.py
@@ -1,6 +1,26 @@
 import tkinter as tk
 
+def handle_generate_button_click(event):
+    """Print the character associated to the key pressed"""
+    print(text_box.get("1.0", tk.END))
+
 window = tk.Tk()
-greeting = tk.Label(text="Hello, Tkinter")
+
+greeting = tk.Label(text="Paste script")
 greeting.pack()
+
+text_box = tk.Text(bg="WHITE SMOKE")
+text_box.pack()
+
+generate_button = tk.Button(
+    text="Generate",
+    width=7,
+    height=2,
+    bg="WHITE SMOKE",
+)
+generate_button.bind("<Button-1>", handle_generate_button_click)
+generate_button.pack()
+
+text_box.get("1.0", tk.END)
+
 window.mainloop()
