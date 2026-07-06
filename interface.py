52fab1e6826597a66a33976dabf185050e7520b6
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-07-05 21:44:59 -0500

    Update layout

--- interface.py
+++ interface.py
@@ -6,11 +6,20 @@ def handle_generate_button_click(event):
 
 window = tk.Tk()
 
-greeting = tk.Label(text="Paste script")
-greeting.pack()
+enter_script = tk.Label(text="Enter script")
+enter_script.grid(row=0, column=0)
 
 text_box = tk.Text(bg="WHITE SMOKE")
-text_box.pack()
+text_box.grid(row=1, column=0)
+
+options = tk.Label(text="Options")
+options.grid(row=0, column=1)
+
+preserve_period = tk.Label(text="Preserve period")
+preserve_period.grid(row=1, column=1)
+
+split_long_sentences = tk.Label(text="Split long sentences")
+split_long_sentences.grid(row=2, column=1)
 
 generate_button = tk.Button(
     text="Generate",
@@ -19,7 +28,7 @@ generate_button = tk.Button(
     bg="WHITE SMOKE",
 )
 generate_button.bind("<Button-1>", handle_generate_button_click)
-generate_button.pack()
+generate_button.grid(row=3, column=1)
 
 text_box.get("1.0", tk.END)
 
