231d6caa85804b3d0e527d9292813ca62cf7d8fd
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-07-06 22:01:38 -0500

    Replace tkinter.Button with tkinter.Checkbutton for options checkboxes

--- interface.py
+++ interface.py
@@ -3,22 +3,10 @@ import tkinter as tk
 def handle_generate_button_click(event):
     """Print the character associated to the key pressed"""
     print(text_box.get("1.0", tk.END))
-    if preserve_period_checkbox["text"] != " ":
-        print("preserve_period_checkbox checked")
-    if split_long_sentences_checkbox["text"] != " ":
-        print("split_long_sentences_checkbox checked")
-
-def handle_preserve_period_checkbox(event):
-    if preserve_period_checkbox["text"] == " ":
-        preserve_period_checkbox.config(text="\u2713")
-    else:
-        preserve_period_checkbox.config(text=" ")
-
-def handle_split_long_sentences_checkbox(event):
-    if split_long_sentences_checkbox["text"] == " ":
-        split_long_sentences_checkbox.config(text="\u2713")
-    else:
-        split_long_sentences_checkbox.config(text=" ")
+    if preserve_period_checkbox_var.get():
+        print("Preserve period")
+    if split_long_sentences_checkbox_var.get():
+        print("Split long sentences")
 
 window = tk.Tk()
 
@@ -33,30 +21,16 @@ text_box.grid(row=1, column=0)
 options = tk.Label(master=options_frame, text="Options:\n")
 options.grid(row=0, column=0)
 
-preserve_period = tk.Label(master=options_frame, text="Preserve period")
-preserve_period.grid(row=1, column=0)
-
-split_long_sentences = tk.Label(master=options_frame, text="Split long sentences")
-split_long_sentences.grid(row=2, column=0)
-
-preserve_period_checkbox = tk.Button(
-    master=options_frame,
-    text=" ",
-    width=1,
-    height=1,
-    bg="WHITE SMOKE",
-)
-preserve_period_checkbox.bind("<Button-1>", handle_preserve_period_checkbox)
+preserve_period_checkbox_var = tk.BooleanVar()
+preserve_period_checkbox = tk.Checkbutton(master=options_frame,
+                                          text="Preserve period",
+                                          variable = preserve_period_checkbox_var)
 preserve_period_checkbox.grid(row=1, column=1)
 
-split_long_sentences_checkbox = tk.Button(
-    master=options_frame,
-    text=" ",
-    width=1,
-    height=1,
-    bg="WHITE SMOKE",
-)
-split_long_sentences_checkbox.bind("<Button-1>", handle_split_long_sentences_checkbox)
+split_long_sentences_checkbox_var = tk.BooleanVar()
+split_long_sentences_checkbox = tk.Checkbutton(master=options_frame,
+                                               text="Split long sentences",
+                                               variable = split_long_sentences_checkbox_var)
 split_long_sentences_checkbox.grid(row=2, column=1)
 
 options_frame.grid(row=0, column=1)
