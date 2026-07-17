170bd5b8603e620da91bc7619e2c0b326a8b9dce
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-07-16 20:28:51 -0500

    Add textboxes for pathname and filename

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import tkinter as tk
+import string_processing as sp
 
 def handle_generate_button_click(event):
-    """Print the character associated to the key pressed"""
-    print(text_box.get("1.0", tk.END))
-    if preserve_period_checkbox_var.get():
-        print("Preserve period")
-    if split_long_sentences_checkbox_var.get():
-        print("Split long sentences")
+    """Generate the vtt file"""
+    text = text_box.get("1.0", tk.END)
+    should_preserve = preserve_period_checkbox_var.get()
+    should_split = split_long_sentences_checkbox_var.get()
+    filename = f""
 
 window = tk.Tk()
 
@@ -32,6 +32,22 @@ split_long_sentences_checkbox = tk.Checkbutton(master=options_frame,
                                                text="Split long sentences",
                                                variable = split_long_sentences_checkbox_var)
 split_long_sentences_checkbox.grid(row=2, column=1)
+
+pathname_label = tk.Label(master=options_frame,
+    text="Pathname")
+pathname_label.grid(row=3, column=1)
+
+pathname_text_box = tk.Entry(master=options_frame, 
+                             width=30,)
+pathname_text_box.grid(row=4, column=1)
+
+filename_label = tk.Label(master=options_frame,
+    text="Filename")
+filename_label.grid(row=5, column=1)
+
+filename_text_box = tk.Entry(master=options_frame, 
+                             width=30,)
+filename_text_box.grid(row=6, column=1)
 
 options_frame.grid(row=0, column=1)
 
--- testing.py
+++ testing.py
@@ -1,12 +1,3 @@
-def split(script: str) -> str:
-    "Break up the script into chunks"
-    return script.split(". ")
+import string_processing
 
-def calculate_times(script: list[str]) -> list[chunks]:
-    pass
-
-#TODO: Add an option for "preserve period"
-
-if (__name__ == "__main__"):
-    r = split("The text: str syntax says that the text argument should be of type str. Similarly, the optional align argument should have type bool with the default value True. Finally, the -> str notation specifies that headline() will return a string.")
-    print(r)
+string_processing.string_processing("stddhidr", "file.vtt")
