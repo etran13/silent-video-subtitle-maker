6fe2a8a2020ee6e5607ec8664c2e5770550e61b6
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-08-14 09:00:30 -0500

    Add Timestamp class with init, str, and add functions (tests incoming)

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import tkinter as tk
 
+class Timestamp:
+    def __init__(self, hrs=0, mins=0, secs=00.000):
+        self.hrs = hrs
+        self.mins = mins
+        self.secs = secs
+
+    def __str__(self):
+        return f"{self.hrs:02}:{self.mins:02}:{self.secs:06.3f}"
+    
+    def add(self, number):
+        "Takes in a number in seconds and adds it to the timestamp."
+        seconds = self.secs + number
+        print(f"Seconds: {seconds}")
+        if seconds < 60:
+            print("Less than")
+            self.secs = seconds
+            return
+        else:
+            carryover = seconds // 60
+            remainder = seconds - (carryover * 60)
+            print(f"Carryover: {carryover}, Remainder: {carryover}")
+            self.secs = remainder
+            self.mins = int(carryover)
+
 def handle_generate_button_click(event):
     """Get all fields and generate the vtt file"""
     text = text_box.get("1.0", tk.END)
@@ -28,58 +52,59 @@ def handle(text, should_preserve, should_split, pathname, filename):
     print(list_of_lines)
     print(list_of_durations)
 
-window = tk.Tk()
+if __name__ == "__main__":
+    window = tk.Tk()
 
-options_frame = tk.Frame()
+    options_frame = tk.Frame()
 
-enter_script = tk.Label(text="Enter script")
-enter_script.grid(row=0, column=0)
+    enter_script = tk.Label(text="Enter script")
+    enter_script.grid(row=0, column=0)
 
-text_box = tk.Text(bg="WHITE SMOKE")
-text_box.grid(row=1, column=0)
+    text_box = tk.Text(bg="WHITE SMOKE")
+    text_box.grid(row=1, column=0)
 
-options = tk.Label(master=options_frame, text="Options:\n")
-options.grid(row=0, column=0)
+    options = tk.Label(master=options_frame, text="Options:\n")
+    options.grid(row=0, column=0)
 
-preserve_period_checkbox_var = tk.BooleanVar()
-preserve_period_checkbox = tk.Checkbutton(master=options_frame,
-                                          text="Preserve period",
-                                          variable = preserve_period_checkbox_var)
-preserve_period_checkbox.grid(row=1, column=1)
+    preserve_period_checkbox_var = tk.BooleanVar()
+    preserve_period_checkbox = tk.Checkbutton(master=options_frame,
+                                            text="Preserve period",
+                                            variable = preserve_period_checkbox_var)
+    preserve_period_checkbox.grid(row=1, column=1)
 
-split_long_sentences_checkbox_var = tk.BooleanVar()
-split_long_sentences_checkbox = tk.Checkbutton(master=options_frame,
-                                               text="Split long sentences",
-                                               variable = split_long_sentences_checkbox_var)
-split_long_sentences_checkbox.grid(row=2, column=1)
+    split_long_sentences_checkbox_var = tk.BooleanVar()
+    split_long_sentences_checkbox = tk.Checkbutton(master=options_frame,
+                                                text="Split long sentences",
+                                                variable = split_long_sentences_checkbox_var)
+    split_long_sentences_checkbox.grid(row=2, column=1)
 
-pathname_label = tk.Label(master=options_frame,
-    text="Pathname")
-pathname_label.grid(row=3, column=1)
+    pathname_label = tk.Label(master=options_frame,
+        text="Pathname")
+    pathname_label.grid(row=3, column=1)
 
-pathname_text_box = tk.Entry(master=options_frame, 
-                             width=30,)
-pathname_text_box.grid(row=4, column=1)
+    pathname_text_box = tk.Entry(master=options_frame, 
+                                width=30,)
+    pathname_text_box.grid(row=4, column=1)
 
-filename_label = tk.Label(master=options_frame,
-    text="Filename")
-filename_label.grid(row=5, column=1)
+    filename_label = tk.Label(master=options_frame,
+        text="Filename")
+    filename_label.grid(row=5, column=1)
 
-filename_text_box = tk.Entry(master=options_frame, 
-                             width=30,)
-filename_text_box.grid(row=6, column=1)
+    filename_text_box = tk.Entry(master=options_frame, 
+                                width=30,)
+    filename_text_box.grid(row=6, column=1)
 
-options_frame.grid(row=0, column=1)
+    options_frame.grid(row=0, column=1)
 
-generate_button = tk.Button(
-    text="Generate",
-    width=7,
-    height=2,
-    bg="WHITE SMOKE",
-)
-generate_button.bind("<Button-1>", handle_generate_button_click)
-generate_button.grid(row=1, column=1)
+    generate_button = tk.Button(
+        text="Generate",
+        width=7,
+        height=2,
+        bg="WHITE SMOKE",
+    )
+    generate_button.bind("<Button-1>", handle_generate_button_click)
+    generate_button.grid(row=1, column=1)
 
-text_box.get("1.0", tk.END)
+    text_box.get("1.0", tk.END)
 
-window.mainloop()
+    window.mainloop()
--- testing.py
+++ testing.py
@@ -1,3 +1,13 @@
-import string_processing
+from main import Timestamp
 
-string_processing.string_processing("stddhidr", "file.vtt")
+test = Timestamp()
+print(test)
+
+# test.add(1)
+# print(f"{test}, {test == "00:00:01.000"}")
+
+test.add(60)
+print(f"{test}, {test == "00:01:00.000"}")
+
+test.add(61)
+print(f"{test}, {test == "00:02:01.000"}")
