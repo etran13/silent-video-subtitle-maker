cb9f7027e104ec26abc66843f2fae0be6c1a6ea3
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-08-15 11:39:53 -0500

    Simplify UI

--- main.py
+++ main.py
@@ -32,14 +32,11 @@ class Timestamp:
 def handle_generate_button_click(event):
     """Get all fields and generate the vtt file"""
     text = text_box.get("1.0", tk.END)
-    should_preserve = preserve_period_checkbox_var.get()
-    should_split = split_long_sentences_checkbox_var.get()
-    pathname = f"{pathname_text_box.get()}"
     filename = f"{filename_text_box.get()}"
 
-    handle(text, should_preserve, should_split, pathname, filename)
+    handle(text, filename)
 
-def handle(text, should_preserve, should_split, pathname, filename):
+def handle(text, filename):
     #Split text into a list of lines
     list_of_lines = text.splitlines()
 
@@ -47,7 +44,7 @@ def handle(text, should_preserve, should_split, pathname, filename):
     time = Timestamp()
     list_of_durations = []
     for line in list_of_lines:
-        duration_seconds = round(len(line) / 16, 3)
+        duration_seconds = round(len(line) / 17, 3)
         start_time = str(time)
         time.add(duration_seconds)
         end_time = str(time)
@@ -72,41 +69,19 @@ if __name__ == "__main__":
     text_box = tk.Text(bg="WHITE SMOKE")
     text_box.grid(row=1, column=0)
 
-    options = tk.Label(master=options_frame, text="Options:\n")
-    options.grid(row=0, column=0)
-
-    preserve_period_checkbox_var = tk.BooleanVar()
-    preserve_period_checkbox = tk.Checkbutton(master=options_frame,
-                                            text="Preserve period",
-                                            variable = preserve_period_checkbox_var)
-    preserve_period_checkbox.grid(row=1, column=1)
-
-    split_long_sentences_checkbox_var = tk.BooleanVar()
-    split_long_sentences_checkbox = tk.Checkbutton(master=options_frame,
-                                                text="Split long sentences",
-                                                variable = split_long_sentences_checkbox_var)
-    split_long_sentences_checkbox.grid(row=2, column=1)
-
-    pathname_label = tk.Label(master=options_frame,
-        text="Pathname")
-    pathname_label.grid(row=3, column=1)
-
-    pathname_text_box = tk.Entry(master=options_frame, 
-                                width=30,)
-    pathname_text_box.grid(row=4, column=1)
-
     filename_label = tk.Label(master=options_frame,
         text="Filename")
     filename_label.grid(row=5, column=1)
 
     filename_text_box = tk.Entry(master=options_frame, 
                                 width=30,)
+    filename_text_box.insert(0, "script")
     filename_text_box.grid(row=6, column=1)
 
     options_frame.grid(row=0, column=1)
 
     generate_button = tk.Button(
-        text="Generate",
+        text="Save",
         width=7,
         height=2,
         bg="WHITE SMOKE",
